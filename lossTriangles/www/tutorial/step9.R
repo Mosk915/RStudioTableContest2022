f58f833d0dcb109dcabe01acfdb06846feb4ece2
COMZM <comzm@rstd03pil.ncci.com>
2022-11-05 23:01:15 -0400

    add code comments

--- lossTriangles/www/tutorial/step9.R
+++ lossTriangles/www/tutorial/step9.R
@@ -1,6 +1,8 @@
 fill_triangle <- function(data_triangle, factors) {
   
-  for(i_col in 2:ncol(data_triangle)) {
+  # iterate through each column in the triangle data frame starting with the 2nd column
+  for(i_col in seq_len(ncol(data_triangle) - 1) + 1) {
+    # for each cell in the column that is missing, take the value in the column preceding it and multiply it by the appropriate development factor
     data_triangle[[i_col]] <- ifelse(is.na(data_triangle[[i_col]]), round(data_triangle[[i_col - 1]] * factors[[i_col - 1]], 0), data_triangle[[i_col]])
   }
   
